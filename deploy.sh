d1d779cd7beb2172fadedc8f4abdd80002785889
Dmitriy Kunin <dkunin@avito.ru>
2025-02-26 09:34:02 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 echo "Starting deployment..."
 
+source /home/dekunin/.bash_profile
+
 # Go to project directory
 cd /var/apps/network-watch || exit
 
@@ -9,9 +11,9 @@ cd /var/apps/network-watch || exit
 git pull origin main
 
 # Install dependencies (optional, if using npm)
-/home/dekunin/.nvm/versions/node/v20.16.0/bin/npm install
+npm install
 
 # Restart pm2 process (replace 'your-pm2-process' with your actual process name)
-/home/dekunin/.nvm/versions/node/v20.16.0/bin/pm2 restart network-watch
+pm2 restart network-watch
 
 echo "Deployment complete!"
